ccbf73415643b0266fb9814a47f39024dccd4ffe
Belzerion <jeille.hd@gmail.com>
2021-04-27 10:42:02 +0200

    benchmarking

---
+++ benchmarking.py
@@ -0,0 +1,119 @@
+
+#%%
+import datetime
+import pyspark as ps
+# Matplotlib
+import matplotlib.pyplot as plt
+
+# Tensorflow
+import tensorflow as tf
+# Numpy and Pandas
+import numpy as np
+import pandas as pd
+# Ohter import
+import sys
+
+from sklearn.preprocessing import StandardScaler
+
+
+# Fashion MNIST
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(images, targets), (images_test, targets_test) = fashion_mnist.load_data()
+
+# Get only a subpart of the dataset
+images = images[:2000]
+targets = targets [:2000]
+
+# Reshape the dataset and convert to float
+images = images.reshape(-1, 784)
+images = images.astype(float)
+images_test = images_test.reshape(-1, 784)
+images_test = images_test.astype(float)
+
+scaler = StandardScaler()
+images = scaler.fit_transform(images)
+images_test = scaler.transform(images_test)
+
+print(images.shape)
+print(targets.shape)
+
+targets_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", 
+                 "Shirt", "Sneaker", "Bag", "Ankle boot"
+]
+
+# Plot one image
+plt.imshow(np.reshape(images[500], (28, 28)), cmap="binary")
+plt.title(targets_names[targets[500]])
+plt.show()
+
+# Flatten
+model = tf.keras.models.Sequential()
+
+# Add the layers
+model.add(tf.keras.layers.Dense(256, activation="relu"))
+model.add(tf.keras.layers.Dense(128, activation="relu"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="relu"))
+model.add(tf.keras.layers.Dense(128, activation="relu"))
+model.add(tf.keras.layers.Dense(128, activation="relu"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(128, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(10, activation="softmax"))
+
+model_output = model.predict(images[0:1])
+print(model_output, targets[0:1])
+
+
+model.summary()
+
+# Compile the model
+model.compile(
+    loss="mse",
+    optimizer="sgd",
+     metrics=[tf.keras.metrics.RootMeanSquaredError()]
+     )
+a = datetime.datetime.now().replace(microsecond=0)
+history = model.fit(images, targets, epochs=100, validation_split=0.2)
+b = datetime.datetime.now().replace(microsecond=0)
+print("computation duration: "+ str(b-a))
+loss_curve = history.history["loss"]
+acc_curve = history.history["root_mean_squared_error"]
+
+loss_val_curve = history.history["val_loss"]
+acc_val_curve = history.history["val_root_mean_squared_error"]
+
+"""
+plt.plot(loss_curve, label="Train")
+#plt.plot(loss_val_curve, label="Val")
+plt.legend(loc='upper left')
+plt.title("Loss")
+plt.show()
+"""
+
+plt.plot(acc_curve, label="Train")
+#plt.plot(acc_val_curve, label="Val")
+plt.legend(loc='upper left')
+plt.title("RMSE")
+plt.show()
+# %%
