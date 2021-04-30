19a8b86e69bf03a8ebbbf244d3ab79d8403d06ce
GBoussard <geoffroy.boussard@insa-lyon.fr>
2021-04-30 11:40:39 +0200

    Add file uploading (supported extensions : txt, py)

--- UI/choix_pool.py
+++ UI/choix_pool.py
@@ -3,6 +3,8 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
+import tkinter as tk  
+from tkinter import filedialog  
 
 st.title('Peer to peer computing')
 st.write("Using peer to peer technology to compute large amount of data")
@@ -10,6 +12,7 @@ st.write("Using peer to peer technology to compute large amount of data")
 
 option = 'Pool 0'
 
+st.header('\nPool selection :')
 
 table = pd.DataFrame({
     'Pools': ['Pool 1','Pool 2','Pool 3','Pool 4'],
@@ -23,3 +26,12 @@ option = st.selectbox(
     'Which pool do you want ?',
     table['Pools'])
 'You selected : ', option
+
+st.header('\nJob file upload :')
+
+# TODO : add more file extensions
+uploaded_file = st.file_uploader("Choose a file", type=['txt', 'py'])
+
+if st.button('Upload'):
+    if uploaded_file is not None:
+        st.write('Uploaded !')
